b55ebced820f26afafc24c7a9bc4011ed52780bc
Michael Nef <michael@nef.net.au>
2020-08-25 11:38:25 +0800

    Added name and fixed makefile

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 
-scheduler: ./src/pipesim.c
-	cc ./src/pipesim.c -Wall -Werror -o ./bin/scheduler
+pipesim: ./src/pipesim.c
+	cc ./src/pipesim.c -Wall -Werror -o ./bin/pipesim
--- src/pipesim.c
+++ src/pipesim.c
@@ -4,8 +4,8 @@
 #include <unistd.h>
 
 /*  CITS2002 Project 1 2020
-    Name:                student-name
-    Student number(s):   student-number
+    Name:                Michael Nefiodovas
+    Student number(s):   22969312
  */
 
 
