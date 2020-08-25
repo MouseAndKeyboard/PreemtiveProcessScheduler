98ecd0e088ea7b783162faf5e58902d17ed5cc52
Michael Nef <michael@nef.net.au>
2020-08-25 12:04:19 +0800

    Formatted file and added test to run

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
 
 pipesim: ./src/pipesim.c
 	cc ./src/pipesim.c -Wall -Werror -o ./bin/pipesim
+
+testrun: pipesim
+	./bin/pipesim
+
+test: testrun
--- src/pipesim.c
+++ src/pipesim.c
@@ -8,167 +8,151 @@
     Student number(s):   22969312
  */
 
-
 //  MAXIMUM NUMBER OF PROCESSES OUR SYSTEM SUPPORTS (PID=1..20)
-#define MAX_PROCESSES                       20
+#define MAX_PROCESSES 20
 
 //  MAXIMUM NUMBER OF SYSTEM-CALLS EVER MADE BY ANY PROCESS
-#define MAX_SYSCALLS_PER_PROCESS            50
+#define MAX_SYSCALLS_PER_PROCESS 50
 
 //  MAXIMUM NUMBER OF PIPES THAT ANY SINGLE PROCESS CAN HAVE OPEN (0..9)
-#define MAX_PIPE_DESCRIPTORS_PER_PROCESS    10
+#define MAX_PIPE_DESCRIPTORS_PER_PROCESS 10
 
 //  TIME TAKEN TO SWITCH ANY PROCESS FROM ONE STATE TO ANOTHER
-#define USECS_TO_CHANGE_PROCESS_STATE       5
+#define USECS_TO_CHANGE_PROCESS_STATE 5
 
 //  TIME TAKEN TO TRANSFER ONE BYTE TO/FROM A PIPE
-#define USECS_PER_BYTE_TRANSFERED           1
-
+#define USECS_PER_BYTE_TRANSFERED 1
 
 //  ---------------------------------------------------------------------
 
 //  YOUR DATA STRUCTURES, VARIABLES, AND FUNCTIONS SHOULD BE ADDED HERE:
 
-int timetaken       = 0;
-
-
+int timetaken = 0;
 
 //  ---------------------------------------------------------------------
 
 //  FUNCTIONS TO VALIDATE FIELDS IN EACH eventfile - NO NEED TO MODIFY
-int check_PID(char word[], int lc)
-{
-    int PID = atoi(word);
+int check_PID(char word[], int lc) {
+  int PID = atoi(word);
 
-    if(PID <= 0 || PID > MAX_PROCESSES) {
-        printf("invalid PID '%s', line %i\n", word, lc);
-        exit(EXIT_FAILURE);
-    }
-    return PID;
+  if (PID <= 0 || PID > MAX_PROCESSES) {
+    printf("invalid PID '%s', line %i\n", word, lc);
+    exit(EXIT_FAILURE);
+  }
+  return PID;
 }
 
-int check_microseconds(char word[], int lc)
-{
-    int usecs = atoi(word);
+int check_microseconds(char word[], int lc) {
+  int usecs = atoi(word);
 
-    if(usecs <= 0) {
-        printf("invalid PID '%s', line %i\n", word, lc);
-        exit(EXIT_FAILURE);
-    }
-    return usecs;
+  if (usecs <= 0) {
+    printf("invalid PID '%s', line %i\n", word, lc);
+    exit(EXIT_FAILURE);
+  }
+  return usecs;
 }
 
-int check_descriptor(char word[], int lc)
-{
-    int pd = atoi(word);
+int check_descriptor(char word[], int lc) {
+  int pd = atoi(word);
 
-    if(pd < 0 || pd >= MAX_PIPE_DESCRIPTORS_PER_PROCESS) {
-        printf("invalid pipe descriptor '%s', line %i\n", word, lc);
-        exit(EXIT_FAILURE);
-    }
-    return pd;
+  if (pd < 0 || pd >= MAX_PIPE_DESCRIPTORS_PER_PROCESS) {
+    printf("invalid pipe descriptor '%s', line %i\n", word, lc);
+    exit(EXIT_FAILURE);
+  }
+  return pd;
 }
 
-int check_bytes(char word[], int lc)
-{
-    int nbytes = atoi(word);
+int check_bytes(char word[], int lc) {
+  int nbytes = atoi(word);
 
-    if(nbytes <= 0) {
-        printf("invalid number of bytes '%s', line %i\n", word, lc);
-        exit(EXIT_FAILURE);
-    }
-    return nbytes;
+  if (nbytes <= 0) {
+    printf("invalid number of bytes '%s', line %i\n", word, lc);
+    exit(EXIT_FAILURE);
+  }
+  return nbytes;
 }
 
 //  parse_eventfile() READS AND VALIDATES THE FILE'S CONTENTS
 //  YOU NEED TO STORE ITS VALUES INTO YOUR OWN DATA-STRUCTURES AND VARIABLES
 void parse_eventfile(char program[], char eventfile[])
 {
-#define LINELEN                 100
-#define WORDLEN                 20
-#define CHAR_COMMENT            '#'
+#define LINELEN 100
+#define WORDLEN 20
+#define CHAR_COMMENT '#'
 
-//  ATTEMPT TO OPEN OUR EVENTFILE, REPORTING AN ERROR IF WE CAN'T
-    FILE *fp    = fopen(eventfile, "r");
+  //  ATTEMPT TO OPEN OUR EVENTFILE, REPORTING AN ERROR IF WE CAN'T
+  FILE *fp = fopen(eventfile, "r");
 
-    if(fp == NULL) {
-        printf("%s: unable to open '%s'\n", program, eventfile);
-        exit(EXIT_FAILURE);
+  if (fp == NULL) {
+    printf("%s: unable to open '%s'\n", program, eventfile);
+    exit(EXIT_FAILURE);
+  }
+
+  char line[LINELEN], words[4][WORDLEN];
+  int lc = 0;
+
+  //  READ EACH LINE FROM THE EVENTFILE, UNTIL WE REACH THE END-OF-FILE
+  while (fgets(line, sizeof line, fp) != NULL) {
+    ++lc;
+
+    //  COMMENT LINES ARE SIMPLY SKIPPED
+    if (line[0] == CHAR_COMMENT) {
+      continue;
     }
 
-    char    line[LINELEN], words[4][WORDLEN];
-    int     lc = 0;
+    //  ATTEMPT TO BREAK EACH LINE INTO A NUMBER OF WORDS, USING sscanf()
+    int nwords = sscanf(line, "%19s %19s %19s %19s", words[0], words[1],
+                        words[2], words[3]);
 
-//  READ EACH LINE FROM THE EVENTFILE, UNTIL WE REACH THE END-OF-FILE
-    while(fgets(line, sizeof line, fp) != NULL) {
-        ++lc;
-
-//  COMMENT LINES ARE SIMPLY SKIPPED
-        if(line[0] == CHAR_COMMENT) {
-            continue;
-        }
-
-//  ATTEMPT TO BREAK EACH LINE INTO A NUMBER OF WORDS, USING sscanf()
-        int nwords = sscanf(line, "%19s %19s %19s %19s",
-                                    words[0], words[1], words[2], words[3]);
-
-//  WE WILL SIMPLY IGNORE ANY LINE WITHOUT ANY WORDS
-        if(nwords <= 0) {
-            continue;
-        }
-
-//  ENSURE THAT THIS LINE'S PID IS VALID
-        int thisPID = check_PID(words[0], lc);
-
-//  OTHER VALUES ON (SOME) LINES
-        int otherPID, nbytes, usecs, pipedesc;
-
-//  IDENTIFY LINES RECORDING SYSTEM-CALLS AND THEIR OTHER VALUES
-//  THIS FUNCTION ONLY CHECKS INPUT;  YOU WILL NEED TO STORE THE VALUES
-        if(nwords == 3 && strcmp(words[1], "compute") == 0) {
-            usecs   = check_microseconds(words[2], lc);
-        }
-        else if(nwords == 3 && strcmp(words[1], "sleep") == 0) {
-            usecs   = check_microseconds(words[2], lc);
-        }
-        else if(nwords == 2 && strcmp(words[1], "exit") == 0) {
-            ;
-        }
-        else if(nwords == 3 && strcmp(words[1], "fork") == 0) {
-            otherPID = check_PID(words[2], lc);
-        }
-        else if(nwords == 3 && strcmp(words[1], "wait") == 0) {
-            otherPID = check_PID(words[2], lc);
-        }
-        else if(nwords == 3 && strcmp(words[1], "pipe") == 0) {
-            pipedesc = check_descriptor(words[2], lc);
-        }
-        else if(nwords == 4 && strcmp(words[1], "writepipe") == 0) {
-            pipedesc = check_descriptor(words[2], lc);
-            nbytes   = check_bytes(words[3], lc);
-        }
-        else if(nwords == 4 && strcmp(words[1], "readpipe") == 0) {
-            pipedesc = check_descriptor(words[2], lc);
-            nbytes   = check_bytes(words[3], lc);
-        }
-//  UNRECOGNISED LINE
-        else {
-            printf("%s: line %i of '%s' is unrecognized\n", program,lc,eventfile);
-            exit(EXIT_FAILURE);
-        }
+    //  WE WILL SIMPLY IGNORE ANY LINE WITHOUT ANY WORDS
+    if (nwords <= 0) {
+      continue;
     }
-    fclose(fp);
 
-#undef  LINELEN
-#undef  WORDLEN
-#undef  CHAR_COMMENT
+    //  ENSURE THAT THIS LINE'S PID IS VALID
+    int thisPID = check_PID(words[0], lc);
+
+    //  OTHER VALUES ON (SOME) LINES
+    int otherPID, nbytes, usecs, pipedesc;
+
+    //  IDENTIFY LINES RECORDING SYSTEM-CALLS AND THEIR OTHER VALUES
+    //  THIS FUNCTION ONLY CHECKS INPUT;  YOU WILL NEED TO STORE THE VALUES
+    if (nwords == 3 && strcmp(words[1], "compute") == 0) {
+      usecs = check_microseconds(words[2], lc);
+    } else if (nwords == 3 && strcmp(words[1], "sleep") == 0) {
+      usecs = check_microseconds(words[2], lc);
+    } else if (nwords == 2 && strcmp(words[1], "exit") == 0) {
+      ;
+    } else if (nwords == 3 && strcmp(words[1], "fork") == 0) {
+      otherPID = check_PID(words[2], lc);
+    } else if (nwords == 3 && strcmp(words[1], "wait") == 0) {
+      otherPID = check_PID(words[2], lc);
+    } else if (nwords == 3 && strcmp(words[1], "pipe") == 0) {
+      pipedesc = check_descriptor(words[2], lc);
+    } else if (nwords == 4 && strcmp(words[1], "writepipe") == 0) {
+      pipedesc = check_descriptor(words[2], lc);
+      nbytes = check_bytes(words[3], lc);
+    } else if (nwords == 4 && strcmp(words[1], "readpipe") == 0) {
+      pipedesc = check_descriptor(words[2], lc);
+      nbytes = check_bytes(words[3], lc);
+    }
+    //  UNRECOGNISED LINE
+    else {
+      printf("%s: line %i of '%s' is unrecognized\n", program, lc, eventfile);
+      exit(EXIT_FAILURE);
+    }
+  }
+  fclose(fp);
+
+#undef LINELEN
+#undef WORDLEN
+#undef CHAR_COMMENT
 }
 
 //  ---------------------------------------------------------------------
 
 //  CHECK THE COMMAND-LINE ARGUMENTS, CALL parse_eventfile(), RUN SIMULATION
-int main(int argc, char *argv[])
-{
-    printf("timetaken %i\n", timetaken);
-    return 0;
+int main(int argc, char *argv[]) {
+  printf("timetaken %i\n", timetaken);
+  return 0;
 }
