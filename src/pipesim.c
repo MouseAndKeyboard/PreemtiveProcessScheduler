69e3c516639bf07cab737265f889cbb769488f21
Michael Nef <michael@nef.net.au>
2020-09-02 19:27:26 +0800

    implemented queue data structure for processes

--- src/pipesim.c
+++ src/pipesim.c
@@ -47,7 +47,7 @@ struct pipe_transmission_t {
 
 // Unions allow me to store different kinds of command data
 // It allows code to remain readable without becoming overcomplicated
-union command_data_t {
+union syscall_data_t {
   int microseconds;
   int pid;
   int descriptor;
@@ -59,21 +59,21 @@ enum process_state_t { ST_READY, ST_RUNNING, ST_WAITING, ST_UNASSIGNED };
 int timetaken = 0;
 struct {
   enum process_state_t state;
-  int next_instruction;
+  int next_syscall;
 
   struct {
     enum syscall_t syscall;
-    union command_data_t data;
-  } instruction_queue[MAX_SYSCALLS_PER_PROCESS];
+    union syscall_data_t data;
+  } syscall_queue[MAX_SYSCALLS_PER_PROCESS];
 
 } process_list[MAX_PROCESSES];
 
 void clear_process(int pid) {
-  process_list[pid].next_instruction = 0;
+  process_list[pid].next_syscall = 0;
 
-  // Initialise all instructions to default value
+  // Initialise all syscalls to default value
   for (int call = 0; call < MAX_SYSCALLS_PER_PROCESS; call++) {
-    process_list[pid].instruction_queue[call].syscall = SYS_UNASSIGNED;
+    process_list[pid].syscall_queue[call].syscall = SYS_UNASSIGNED;
   }
 }
 
@@ -83,9 +83,51 @@ void initialise_process_list() {
   }
 }
 
+// Helper functions for the ready queue
+bool queue_empty(int head, int tail) { return head == tail; }
+int dequeue(int *queue, int *head) { return queue[(*head)++]; }
+void enqueue(int *queue, int *tail, int value) { queue[(*tail)++] = value; }
+
+int rdy_head = 0;
+int rdy_tail = 0;
+int rdy_queue[MAX_PROCESSES];
+
+#define STATE_NAME_LEN 15
+
 void state_transition(int pid, enum process_state_t new_state) {
   process_list[pid].state = new_state;
-  timetaken += USECS_PER_BYTE_TRANSFERED;
+
+  char state[STATE_NAME_LEN];
+  switch (new_state) {
+  case ST_READY: {
+    snprintf(state, sizeof(state), "READY");
+    break;
+  }
+  case ST_RUNNING: {
+    snprintf(state, sizeof(state), "RUNNING");
+    break;
+  }
+  case ST_WAITING: {
+    snprintf(state, sizeof(state), "WAITING");
+    break;
+  }
+  case ST_UNASSIGNED: {
+    snprintf(state, sizeof(state), "REMOVED");
+    break;
+  }
+  default: {
+    snprintf(state, sizeof(state), "!UNKNOWN!");
+    break;
+  }
+  }
+
+  printf("[#] Changing process_%i's state -> %s\n", pid, state);
+  if (ST_READY == new_state) {
+    // make sure to add processes which are ready to the "ready" queue
+    enqueue(rdy_queue, &rdy_tail, pid);
+  }
+
+  timetaken += USECS_TO_CHANGE_PROCESS_STATE;
 }
 
 void sim_exit(int pid) {
@@ -93,19 +135,73 @@ void sim_exit(int pid) {
   state_transition(pid, ST_UNASSIGNED);
 }
 
+void sim_compute(int pid, int time_quantum) {
+  int syscall_index = process_list[pid].next_syscall;
+
+  int usecs = process_list[pid].syscall_queue[syscall_index].data.microseconds;
+
+  if (usecs > time_quantum) {
+    timetaken += time_quantum;
+    usecs -= time_quantum;
+  } else {
+    timetaken += usecs;
+    // We have finished computing
+    usecs = 0;
+    ++process_list[pid].next_syscall;
+  }
+
+  process_list[pid].syscall_queue[syscall_index].data.microseconds = usecs;
+  state_transition(pid, ST_READY);
+}
+
+void sim_fork(int parent_pid) {
+  int syscall_index = process_list[parent_pid].next_syscall;
+  // subtract 1 to get the process index from 0 (rather than from 1)
+  int child_pid =
+      process_list[parent_pid].syscall_queue[syscall_index].data.pid - 1;
+
+  // In reality the child would inherit all the
+  // state of the parent, but since there's no
+  // state for these processes.
+
+  ++process_list[parent_pid].next_syscall;
+
+  state_transition(child_pid, ST_READY);
+  state_transition(parent_pid, ST_READY);
+}
+
 // Uses the values in the command_queue to find and set
 // the timetaken global variable.
 void run_simulation(int time_quantum, int pipe_buff_size) {
 
   int running = 0; // PID=1 ( index starts at 0 ) always first process
-  process_list[running].state = ST_RUNNING;
+  bool done = false;
 
+  process_list[running].state = ST_RUNNING;
   do {
-    int syscall_idex = process_list[running].next_instruction;
-    switch (process_list[running].instruction_queue[syscall_idex].syscall) {
+
+    // Get the system call of the currently executing process
+    int syscall_index = process_list[running].next_syscall;
+
+    printf("[%iusecs] [syscall %i] \t process_%i::", timetaken, syscall_index,
+           running);
+
+    // Decide how to process the particular system call
+    switch (process_list[running].syscall_queue[syscall_index].syscall) {
     case SYS_EXIT: {
+      printf("exit();\n");
       sim_exit(running);
-      // must find new process to run
+      done = true;
+      break;
+    }
+    case SYS_COMPUTE: {
+      printf("compute();\n");
+      sim_compute(running, time_quantum);
+      break;
+    }
+    case SYS_FORK: {
+      printf("fork();\n");
+      sim_fork(running);
       break;
     }
     default: {
@@ -113,12 +209,16 @@ void run_simulation(int time_quantum, int pipe_buff_size) {
     }
     }
 
-    for (int pid = 0; pid < MAX_PROCESSES; pid++) {
-      if (process_list[pid].state == ST_READY) {
-      }
+    // if there are still processes running, we need to select the next
+    // one to compute.
+    if (!queue_empty(rdy_head, rdy_tail)) {
+      // select the process at the front of the queue
+      running = dequeue(rdy_queue, &rdy_head);
+      state_transition(running, ST_RUNNING);
+      done = false;
     }
 
-  } while (true); // until simulation is complete
+  } while (!done); // until simulation is complete
 }
 
 //  ---------------------------------------------------------------------
@@ -208,76 +308,67 @@ void parse_eventfile(char program[], char eventfile[]) {
     //  Storing values inside the command structure.
 
     int pid_index = thisPID - 1;
-    int next_instruction = process_list[pid_index].next_instruction;
+    int next_syscall = process_list[pid_index].next_syscall;
 
     if (nwords == 3 && strcmp(words[1], "compute") == 0) {
 
-      process_list[pid_index].instruction_queue[next_instruction].syscall =
-          SYS_COMPUTE;
+      process_list[pid_index].syscall_queue[next_syscall].syscall = SYS_COMPUTE;
 
-      process_list[pid_index]
-          .instruction_queue[next_instruction]
-          .data.microseconds = check_microseconds(words[2], lc);
+      process_list[pid_index].syscall_queue[next_syscall].data.microseconds =
+          check_microseconds(words[2], lc);
 
     } else if (nwords == 3 && strcmp(words[1], "sleep") == 0) {
 
-      process_list[pid_index].instruction_queue[next_instruction].syscall =
-          SYS_SLEEP;
+      process_list[pid_index].syscall_queue[next_syscall].syscall = SYS_SLEEP;
 
-      process_list[pid_index]
-          .instruction_queue[next_instruction]
-          .data.microseconds = check_microseconds(words[2], lc);
+      process_list[pid_index].syscall_queue[next_syscall].data.microseconds =
+          check_microseconds(words[2], lc);
 
     } else if (nwords == 2 && strcmp(words[1], "exit") == 0) {
 
-      process_list[pid_index].instruction_queue[next_instruction].syscall =
-          SYS_EXIT;
+      process_list[pid_index].syscall_queue[next_syscall].syscall = SYS_EXIT;
 
     } else if (nwords == 3 && strcmp(words[1], "fork") == 0) {
 
-      process_list[pid_index].instruction_queue[next_instruction].syscall =
-          SYS_FORK;
+      process_list[pid_index].syscall_queue[next_syscall].syscall = SYS_FORK;
 
-      process_list[pid_index].instruction_queue[next_instruction].data.pid =
+      process_list[pid_index].syscall_queue[next_syscall].data.pid =
           check_PID(words[2], lc);
 
     } else if (nwords == 3 && strcmp(words[1], "wait") == 0) {
-      process_list[pid_index].instruction_queue[next_instruction].syscall =
-          SYS_WAIT;
+      process_list[pid_index].syscall_queue[next_syscall].syscall = SYS_WAIT;
 
-      process_list[pid_index].instruction_queue[next_instruction].data.pid =
+      process_list[pid_index].syscall_queue[next_syscall].data.pid =
           check_PID(words[2], lc);
     } else if (nwords == 3 && strcmp(words[1], "pipe") == 0) {
-      process_list[pid_index].instruction_queue[next_instruction].syscall =
-          SYS_PIPE;
+      process_list[pid_index].syscall_queue[next_syscall].syscall = SYS_PIPE;
 
-      process_list[pid_index]
-          .instruction_queue[next_instruction]
-          .data.descriptor = check_descriptor(words[2], lc);
+      process_list[pid_index].syscall_queue[next_syscall].data.descriptor =
+          check_descriptor(words[2], lc);
 
     } else if (nwords == 4 && strcmp(words[1], "writepipe") == 0) {
-      process_list[pid_index].instruction_queue[next_instruction].syscall =
+      process_list[pid_index].syscall_queue[next_syscall].syscall =
           SYS_WRITEPIPE;
 
       process_list[pid_index]
-          .instruction_queue[next_instruction]
+          .syscall_queue[next_syscall]
           .data.pipe_info.descriptor = check_descriptor(words[2], lc);
 
       process_list[pid_index]
-          .instruction_queue[next_instruction]
+          .syscall_queue[next_syscall]
           .data.pipe_info.nbytes = check_bytes(words[3], lc);
 
     } else if (nwords == 4 && strcmp(words[1], "readpipe") == 0) {
 
-      process_list[pid_index].instruction_queue[next_instruction].syscall =
+      process_list[pid_index].syscall_queue[next_syscall].syscall =
           SYS_READPIPE;
 
       process_list[pid_index]
-          .instruction_queue[next_instruction]
+          .syscall_queue[next_syscall]
           .data.pipe_info.descriptor = check_descriptor(words[2], lc);
 
       process_list[pid_index]
-          .instruction_queue[next_instruction]
+          .syscall_queue[next_syscall]
           .data.pipe_info.nbytes = check_bytes(words[3], lc);
     }
     // UNRECOGNISED LINE
@@ -286,9 +377,13 @@ void parse_eventfile(char program[], char eventfile[]) {
       exit(EXIT_FAILURE);
     }
     ++process_list[pid_index]
-          .next_instruction; // Make sure to always assign to a new location
+          .next_syscall; // Make sure to always assign to a new location
   }
   fclose(fp);
+
+  for (int pid_index = 0; pid_index < MAX_PROCESSES; pid_index++) {
+    process_list[pid_index].next_syscall = 0;
+  }
 
 #undef LINELEN
 #undef WORDLEN
@@ -323,9 +418,13 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "[#] need to check valid range of buffer size\n");
   }
 
+  printf("[#] Initialising process list\n");
   initialise_process_list(); // initialising process list separately from
                              // parsing. Each function should be self-contained
+  printf("[#] Parsing eventfile...\n");
   parse_eventfile(argv[0], argv[1]);
+
+  printf("[#] Running simulation...\n");
   run_simulation(quant, pipe_buff_size);
 
   printf("timetaken %i\n", timetaken);
